6b63870b3a0ca8c116f365ce32cf68b64d203842
Seungpil Lee <iamseungpil@gm.gist.ac.kr>
2024-11-13 05:21:19 -0500

    gh-pages

---
+++ _app/immutable/entry/start.ExqkqknJ.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.j8oZ8MHt.js";export{t as start};
--- svelte.config.js
+++ svelte.config.js
@@ -12,7 +12,7 @@ const config = {
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
 		adapter: adapter({
-			fallback: '404.html'
+			fallback: 'index.html'
 		}),
 		paths: {
 			base: process.env.NODE_ENV === 'production' ? '/mc-larc.github.io' : ''
